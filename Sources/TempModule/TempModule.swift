fb8d1a66c10da12d18c47b32bc10990de8376ba9
원형식 <hyeongsik.won@navercorp.com>
2023-10-10 19:41:49 +0900

    test

--- Sources/TempModule/TempModule.swift
+++ Sources/TempModule/TempModule.swift
@@ -1,7 +1,11 @@
+import CommonUtility
+
 public struct TempModule {
-    public private(set) var text = "Hello, World!"
+    public private(set) var text = "This is TempModule"
 
     public init() {
         print(text)
+        
+        CommonUtility.testRun()
     }
 }
